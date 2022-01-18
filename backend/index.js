46e35cd27b465f9e04a357eb2314794729569e2a
Razvan Tamsa <razvan.tamsa@opswat.com>
2022-01-18 13:22:55 +0200

    build crud routes

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,75 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 const cors = require('cors');
-// eslint-disable-next-line no-unused-vars
 const pool = require('./db');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // req.body
 
 // ROUTES
 // create a todo
+app.post('/todos', async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query('INSERT INTO todo(description) VALUES($1)', [description]);
+    res.send(newTodo);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // get all todos
 
+app.get('/todos', async (req, res) => {
+  try {
+    const allTodos = await pool.query('SELECT * FROM todo ORDER BY todo_id');
+    res.json(allTodos.rows);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 // get a todo
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1', [id]);
+    res.json(todo.rows[0]);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // update a todo
+app.put('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query('UPDATE todo SET description = $1 WHERE todo_id = $2', [description, id]);
+    res.json('Todo was updated!');
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // delete a todo
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query('DELETE FROM todo WHERE todo_id = $1', [id]);
+    res.json('Todo was deleted');
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`Server running on PORT ${PORT}`);
 });
