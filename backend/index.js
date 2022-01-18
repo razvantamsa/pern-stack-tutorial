c94d191759667030882940c3c3408ed406799a4d
Razvan Tamsa <razvan.tamsa@opswat.com>
2022-01-18 12:31:31 +0200

    connect db and server

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,25 @@ const express = require('express');
 
 const app = express();
 const cors = require('cors');
+// eslint-disable-next-line no-unused-vars
+const pool = require('./db');
 
 const PORT = 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// ROUTES
+// create a todo
+
+// get all todos
+
+// get a todo
+
+// update a todo
+
+// delete a todo
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
